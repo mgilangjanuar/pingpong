7ad80b97a61e3607a16f2e3dd9f60f1bc7a985c8
M Gilang Januar <mgilangjanuar@gmail.com>
2020-04-13 16:05:41 +0700

    init with basic feature

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,44 @@
 import 'source-map-support/register'
-import express from 'express'
+
+import compression from 'compression'
+import express, { Request, Response } from 'express'
+import pug from 'pug'
+import morgan from 'morgan'
+import bodyParser from 'body-parser'
+import { DB } from './Service/DB'
+import { Web } from './Web'
+import { runWorker } from './Service/Worker'
+
+// initiate database
+DB.init()
+
+// run worker
+runWorker()
 
 const app = express()
-app.get('/ping', (_, res) => res.send({ pong: 1 }))
+app.use(compression())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('./assets'))
+app.use(morgan('combined'))
 
-app.listen(4001, () => console.log('Run at http://localhost:4001...'))
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore
+app.engine('pug', pug.__express)
+app.set('view engine', 'pug')
+
+app.get('/ping', (_, res) => res.send({ pong: 1 }))
+app.use('', Web())
+
+// app.use((e: { status: number, body: object }, _: Request, s: Response) => {
+//   // console.log(e)
+//   if (e.status && e.body) {
+//     return s.status(e.status).send(e.body)
+//   } else {
+//     return s.status(500).send({ reason: '500 Internal Server Error', error: e.toString() })
+//   }
+// })
+
+// app.use((_, res: Response, __) => res.status(404).send({ msg: 'No Route Found' }))
+
+app.listen(process.env.PORT || 4001, () => console.log(`Run at http://localhost:${process.env.PORT || 4001} ...`))
