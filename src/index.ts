2cc044996973a60be5597db748c4096955492f7f
M Gilang Januar <mgilangjanuar@gmail.com>
2020-04-13 11:32:24 +0700

    init express app with its dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('test')
+import 'source-map-support/register'
+import express from 'express'
+
+const app = express()
+app.get('/ping', (_, res) => res.send({ pong: 1 }))
+
+app.listen(4001, () => console.log('Run at http://localhost:4001...'))
